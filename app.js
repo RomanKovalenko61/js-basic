e3b6339b9861b7090fd7ad1a7a014c8a8fb1a1d2
Roman <astek14@mail.ru>
2024-04-17 17:25:05 +0300

    4.9. False из других типов

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const age = '18';
-console.log(Number(age) + 5);
-console.log(age - 3);
-const userName = 'Вася';
-console.log(Number(userName) + 5);
-console.log(typeof NaN);
-console.log(String(4) + 7);
-console.log(Boolean(-1)); // 0 или пустая строка - false остальные числа и непустые строки true
-console.log(Boolean('') + 10);
-console.log(true + 2);
-
-
-const a = 2 + '10';
-console.log(a - 10);
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('sx')));
+console.log(Boolean(NaN));
