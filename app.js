389645a786e79edd16bd9905e338f00d280cb51d
Roman <astek14@mail.ru>
2024-04-17 15:50:50 +0300

    4.3. Операторы присваивания и сравнения

--- app.js
+++ app.js
@@ -14,3 +14,25 @@ console.log(volume);
 const city = 'Москва'
 const street = 'Новослободская';
 console.log(city + ', ' + street + ' ' + 5);
+
+//Операторы присваивания
+let age = 18 + 5;
+
+age += 2; // age = age + 2
+age -= 3; // age = age - 3
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
+
+age++; // age = age + 1
+age--; // age = age - 1
+
+console.log(age);
+
+//Операторы сравнения
+const vasya = 20;
+
+console.log(age > vasya);
+console.log(age >= vasya);
+console.log(age < vasya);
+console.log(age <= vasya);
+console.log(age == vasya);
