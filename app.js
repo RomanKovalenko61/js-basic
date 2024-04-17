2e364f4f3fe1dd0a6f825548bcc91807df22fb8e
Roman <astek14@mail.ru>
2024-04-17 16:06:33 +0300

    4.4. Порядок операторов

--- app.js
+++ app.js
@@ -36,3 +36,20 @@ console.log(age >= vasya);
 console.log(age < vasya);
 console.log(age <= vasya);
 console.log(age == vasya);
+
+//Порядок операторов
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
+
+// 100 - (11) 10 > (9) 90 - (11) 5
+// Приоритет операторов
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+
+const a = (6 + 10) / 2;
+console.log(a);
+
+let b;
+let c;
+c = b = 100 + 50;
+console.log(c);
+console.log(b);
