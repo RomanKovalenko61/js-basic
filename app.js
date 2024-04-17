dd6b28587defc89ff52b0d8d080f2763bb4b0272
Roman <astek14@mail.ru>
2024-04-17 17:01:09 +0300

    4.7. Шаблонные строки

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-/* 
-Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 
-5 дней в неделю (кроме выходных).К вам приходит заказчик 
-и предлагает заказ на 40 часов работы. Сейчас понедельник. 
-Вы должны уехать через 11 дней.Выведете в консоль:
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
 
- - Boolean переменную успеете ли вы взяться за работу
- - Сколько вы за неё попросите?
-*/
+const template = author + ' заказал ' + projectName  + ' по цене ' + price + '$';
+console.log(template);
 
-const payment = 80;
-const work = 40;
-let workHours = (11 - 2) * 5;
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2);
 
-console.log('Смогу ли я работать? ' + (workHours > work));
-console.log('Стоимость работ: ' + payment * work);
+const template3 = 'Проект \n' + 'Цена: ' + price + '$';
+console.log(template3);
+
+const template4 = `Проект
+Цена: ${price}$`;
+console.log(template4);
