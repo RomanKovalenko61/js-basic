fd9c5183d9a41452e9b810d1b15425aca9c8d3cc
Roman <astek14@mail.ru>
2024-04-23 00:01:25 +0300

    8.8. Деструктуризация

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
-/*
-    Даг список задач
-    const tasks = ['Задача 1'];
-    Сделать функции:
-    - Добавиление задачи в конец
-    - Удаление задачи по названию
-    - Перенос задачи в начало списка по названию
-    Всегда меняем исходный массив
-*/
+const userData = ['Антон', 18, 'Москва'];
 
-const tasks = ['Задача 1'];
-
-function addTask(task){
-    tasks.push(task);
+function getData() {
+    return ['Антон', 18, 'Москва'];
 }
 
-function deleteTask(task) {
-    const index = tasks.indexOf(task);
-    if (index === -1) {
-        return;
-    }
-    return tasks.splice(index, 1);
-}
+// const userName = getData()[0];
+// const age = getData()[1];
+// const city = getData()[2];
 
-function priorityTask(task){
-    const result = deleteTask(task);
-    if (!result) {
-        return;
-    }
-    tasks.unshift(result[0]);
-}
+//const [userName, age, city] = userData;
+const [userName, _, city] = getData();
 
-addTask('Задача 2');
-addTask('Задача 3');
-console.log(tasks);
-
-deleteTask('Задача 2');
-console.log(tasks);
-deleteTask('Задача 4');
-console.log(tasks);
-priorityTask('Задача 3');
-console.log(tasks);
-priorityTask('Задача 4');
-console.log(tasks);
-
+console.log(userName, city);
