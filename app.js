27662394f573eb8b7c6c4b08fe803a73d62e6e6a
Roman <astek14@mail.ru>
2024-04-22 23:00:37 +0300

    8.6. Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+    Даг список задач
+    const tasks = ['Задача 1'];
+    Сделать функции:
+    - Добавиление задачи в конец
+    - Удаление задачи по названию
+    - Перенос задачи в начало списка по названию
+    Всегда меняем исходный массив
+*/
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
+const tasks = ['Задача 1'];
 
-console.log(roles.join('-'));
+function addTask(task){
+    tasks.push(task);
+}
+
+function deleteTask(task) {
+    const index = tasks.indexOf(task);
+    if (index === -1) {
+        return;
+    }
+    tasks.splice(index, 1);
+}
+
+function priorityTask(task){
+    const index = tasks.indexOf(task);
+    if (index === -1) {
+        return;
+    }
+    const oldTask = tasks[index];
+    tasks.splice(index, 1);
+    tasks.unshift(task);
+}
+
+addTask('Задача 2');
+addTask('Задача 3');
+console.log(tasks);
+
+deleteTask('Задача 2');
+console.log(tasks);
+deleteTask('Задача 4');
+console.log(tasks);
+priorityTask('Задача 3');
+console.log(tasks);
+priorityTask('Задача 4');
+console.log(tasks);
+
