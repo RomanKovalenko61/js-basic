eb5470e27ebb51693c8a1f8642b5a22836e2c838
Roman <astek14@mail.ru>
2024-04-23 17:20:24 +0300

    9.7. Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
-const arr = [1, 4, 8, 7];
+/*
+    Есть выгрузка операций пользователя
+    const operations = [1000, -700, 300, -500, 10000];
+    а также начальный баланс в 100$
+    Необходимо сделать функции расчета:
+    - Итогового баланса
+    - Наличия отрицательного баланса (если после очередной операции
+        баланс < 0, то выдавать false)
+    - Расчета среднего расхода и среднего дохода
+*/
+const operations = [1000, -700, 300, -500, 10000];
+const startingBalance = 100;
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+function getBalance(arrayOfOperations, initialBalance) {
+    let balance = initialBalance;
+    for(const element of arrayOfOperations) {
+        balance += element;
+    }
+    return balance;
 }
 
-for (let element of arr) {
-    console.log(element);
+function checkOperations(arrayOfOperations, startingBalance) {
+    let balance = startingBalance;
+    let isPositive = true;
+    for(const element of arrayOfOperations) {
+        balance += element;
+        if (balance < 0) {
+            isPositive = false;
+            return isPositive;
+        }
+    }
+    return isPositive;
 }
 
-for (let index in arr) {
-    console.log(index);
-    console.log(arr[index]);
+function averageOperations(arrayOfOperations) {
+    let positiveCount = 0;
+    let positiveSum = 0;
+    let negativeCount = 0;
+    let negativeSum = 0;
+
+    for (const element of arrayOfOperations) {
+        if (element > 0) {
+            positiveSum += element;
+            positiveCount++;
+        } if (element < 0) {
+            negativeSum += element;
+            negativeCount++;
+        }
+    }
+    return [positiveSum / positiveCount, negativeSum / negativeCount];
 }
+
+console.log('Итоговый баланс: ' + getBalance(operations, startingBalance));
+console.log('Проверка на положительный баланс: ' + checkOperations(operations, startingBalance));
+
+const result = averageOperations(operations);
+
+console.log(`Средний доход: ${result[0]}`);
+console.log(`Средний расход: ${result[1]}`);
