c266b800a640abc5a2650e8078de3ae00b3ec7f1
Roman <astek14@mail.ru>
2024-05-09 21:11:41 +0300

    10.4. Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-function power(pow) {
-    return function (num) {
-        return num ** pow;
-    };
-}
+// function power(pow) {
+//     return function (num) {
+//         return num ** pow;
+//     };
+// }
 
-const powerOfTwo = power(2);
-console.log(powerOfTwo(5));
-console.log(powerOfTwo(10));
-
-const powerOfThree = power(3);
-console.log(powerOfThree(5));
-
-console.log(power(5)(4));
+const power = (pow) => (num) => num ** pow;
+console.log(power(5)(2)); // Ожидаемый вывод: 32 (2^5 = 32)
