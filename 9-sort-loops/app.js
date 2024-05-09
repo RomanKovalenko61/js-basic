883a46e2b98d7bc54b5c49a7b9c340b66443266f
Roman <astek14@mail.ru>
2024-05-09 18:50:07 +0300

    Исправил изменение исходного массива

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,27 +1,27 @@
 const arr = [1, 40, -5, 10, 0];
 
 console.log("Первоначальный массив " + arr);
-console.log("Отсортированный по возрастанию " + greedySort(arr, 0));
-console.log("Отсортированный по убыванию " + greedySort(arr, 1));
+console.log("Отсортированный по возрастанию " + sortArrayInAscending(arr));
+console.log("Первоначальный массив " + arr);
+console.log("Отсортированный по убыванию " + sortArrayInAscending(arr, 1));
+console.log("Первоначальный массив " + arr);
 
-function greedySort(sorted, isReverse) {
-    for (let i = 0; i < sorted.length; i++) {
-        let lastIndex = i;
-        const curr = sorted[i];
-        for (let j = i + 1; j < sorted.length; j++) {
-            if (curr > sorted[j]) {
-                sorted[lastIndex] = sorted[j];
-                lastIndex = j;
-                sorted[j] = curr;
+function orderTemplate(firstNumber, secondNumber, desc) {
+    if (!desc) {
+        return firstNumber > secondNumber;
+    }
+    return firstNumber < secondNumber;
+}
+
+function sortArrayInAscending(arr, desc = false) {
+    const array = arr.slice(0);
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            const isExchange = orderTemplate(array[i], array[j], desc);
+            if (isExchange) {
+                [array[i], array[j]] = [array[j], array[i]];
             }
         }
     }
-    if (isReverse) {
-        const reverseSortedArr = [];
-        for (let i = arr.length - 1; i >= 0; i--) {
-            reverseSortedArr.push(sorted.pop());
-        }
-        return reverseSortedArr;
-    }
-    return arr;
+    return array;
 }
