6a509761b219395da6d6b50fac909e4f37451851
Roman <astek14@mail.ru>
2024-05-11 22:36:37 +0300

    12.4. Преобразование строки

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-/* Вытащить имя и фамилию в отдельные переменные */
-const fullUserName = "Вася aka Terminator Perfinator Пупкин";
+const str = "Вася Пупкина";
+console.log(str.includes("а"));
+console.log(str.startsWith("В"));
+console.log(str.endsWith("н"));
 
-const userName = fullUserName.slice(0, fullUserName.indexOf(" "));
-console.log(userName);
-const surName = fullUserName.slice(
-    fullUserName.lastIndexOf(" ") + 1,
-    fullUserName.length
-);
-console.log(surName);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace("В", "Ф"));
+console.log(str.replace("П", "Д"));
+console.log(str.replace("а", "и"));
+console.log(str.replaceAll("а", "и")); // работает не во всех браузерах
+
+console.log(str.replace(/а/g, "и")); //regexp
+
+const str2 = " Вася    Пупкина  \n";
+console.log(str2.trim());
+console.log(str2.trimStart());
+console.log(str2.trimEnd());
