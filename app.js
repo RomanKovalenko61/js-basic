59463bf7dd2513d7704537e2bb769779ab0bdcf3
Roman <astek14@mail.ru>
2024-05-25 20:53:35 +0300

    13.2. Обращение к элементам

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const userArray = ["VAsya", "Pupkin", 24];
 const user = {
     name: "Vasya",
     surname: "Pupkin",
     age: 24,
     skills: ["Programming", "Cooking"],
-    1: 1,
+    eduBasic: "School 10",
+    eduPro: "MFTI",
 };
 
+console.log(user);
+console.log(user.name);
+console.log(user.skills[0]);
+
+console.log(user["skills"]);
+
+const level = "Pro";
+console.log(user["edu" + level]);
+
+// const res = prompt("Введите свойство");
+// console.log(user[res]);
+
+user["city"] = "Москва";
+console.log(user);
+
+user.age = 30;
+user["age"] = 40;
 console.log(user);
