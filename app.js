e89a13ce69391a6d77028241bc0c77328306dfc4
Roman <astek14@mail.ru>
2024-05-25 21:29:36 +0300

    13.5. Методы объектов

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-/* преобразовать пользователей до вида 
-    { fullName: 'Вася Пупкин', skillNum: 2}
-*/
-
-const users = [
-    {
-        name: "Вася",
-        surname: "Пупкин",
-        age: 30,
-        skills: ["Программирование", "DevOps"],
+const user = {
+    name: "Вася",
+    surname: "Пупкин",
+    age: 24,
+    getFullName: function () {
+        console.log(this);
+        return this.name + " " + this.surname;
     },
-    {
-        name: "Катя",
-        surname: "Белова",
-        age: 18,
-        skills: ["Дизайн"],
-    },
-];
+};
 
-console.log(
-    users.map((obj) => {
-        return {
-            fullName: `${obj.name} ${obj.surname}`,
-            skillNum: obj.skills.length,
-        };
-    })
-);
+console.log(user.getFullName());
