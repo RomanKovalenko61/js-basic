3a3071935e2fd89860add2d37d06041e3638a362
Roman <astek14@mail.ru>
2024-05-26 16:32:11 +0300

    Mapping obj to query string

---
+++ 13-query/app.js
@@ -0,0 +1,27 @@
+const obj = {
+    search: "Вася",
+    take: 10,
+};
+
+function createQuery(obj) {
+    let query = "";
+    const arr = Object.keys(obj);
+    for (let i = 0; i < arr.length; i++) {
+        query += arr[i] + "=" + obj[arr[i]];
+        if (i != arr.length - 1) {
+            query += "&";
+        }
+    }
+    return query;
+}
+
+function createQueryAlt(obj) {
+    let query = "";
+    for (const key of Object.keys(obj)) {
+        query += key + "=" + obj[key] + "&";
+    }
+    return query.slice(0, -1);
+}
+
+console.log(createQuery(obj));
+console.log(createQueryAlt(obj));
