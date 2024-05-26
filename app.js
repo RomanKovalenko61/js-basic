b08187607258c09e26112de6ed5e6d2e73a25a27
Roman <astek14@mail.ru>
2024-05-26 14:33:22 +0300

    13.8. Итерирование по объекту

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const initialBalance = 7;
-
-const wallet = {
-    balance: initialBalance,
-    operations: [],
+const cities = {
+    msk: {
+        let: 200,
+        temp: 25,
+    },
+    spb: {
+        let: 100,
+        temp: 20,
+    },
 };
 
-console.log(wallet);
+let sumTemp = 0;
+console.log(Object.keys(cities));
+let citiesCount = Object.keys(cities).length;
+// for (const key in cities) {
+//     sumTemp += cities[key].temp;
+// }
+// console.log(sumTemp / citiesCount);
+
+for (const key of Object.keys(cities)) {
+    sumTemp += cities[key].temp;
+}
+console.log(sumTemp / citiesCount);
