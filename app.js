69eb7b62ea5468bec8f849bfbc68732cfe07b5a0
Roman <astek14@mail.ru>
2024-06-04 21:07:13 +0300

    15.10. Объект в объекте

--- app.js
+++ app.js
@@ -1,59 +1,31 @@
 /* 
-    Сделать объект склад с методами добавления на склад, поиска 
-    по складу товара и расчет веса
+    Дополнить объект методами для получения имени:
+    - компании
+    - ceo
+    - сотрудника
 */
 
-const warehouse = {
-    goods: [],
-    findGoodById: function (id) {
-        return (existGood = this.goods.find((g) => g.id === id));
+const company = {
+    name: "ООО Агро",
+    employees: [
+        {
+            name: "Света",
+            getName: function () {
+                return this.name;
+            },
+        },
+    ],
+    ceo: {
+        name: "Вася",
+        getName: function () {
+            return this.name;
+        },
     },
-    addGood: function (good) {
-        const existGood = this.findGoodById(good.id);
-        if (existGood) {
-            console.log("Этот товар уже есть на складе");
-            return;
-        }
-        this.goods.push(good);
-    },
-    getWeightKg: function () {
-        return this.goods.reduce(
-            (acc, current) =>
-                (acc += current.weight?.kg ? current.weight.kg : 0),
-            0
-        );
-    },
-};
-
-/* Товары */
-const car = {
-    id: 1,
-    weight: {
-        kg: 1000,
-    },
-    brand: "Ford",
-};
-
-const chair = {
-    id: 2,
-    weight: {
-        kg: 2,
+    getName: function () {
+        return this.name;
     },
 };
 
-const paper = {
-    id: 3,
-    color: "red",
-};
-
-warehouse.addGood(car);
-warehouse.addGood(car);
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-
-console.log(warehouse.goods);
-console.log(warehouse.findGoodById(6));
-console.log(warehouse.findGoodById(1));
-console.log(warehouse.findGoodById(2));
-console.log(warehouse.findGoodById(3));
-console.log(warehouse.getWeightKg());
+console.log(company.getName());
+console.log(company.ceo.getName());
+console.log(company.employees.map((e) => e.getName()));
