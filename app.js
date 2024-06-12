6a5c80d3012453505d595ac7f887105244be4923
Roman <astek14@mail.ru>
2024-06-12 23:46:31 +0300

    17.6. Установка атрибутов

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ function submitForm() {
     }
     document.querySelector(".panel").innerText = input;
     document.querySelector(".input").value = "";
+    console.log(document.querySelector(".notification").getAttribute("class"));
+    document
+        .querySelector(".notification")
+        .setAttribute("class", "notification");
+    document.querySelector(".notification").setAttribute("key", 1);
+    document.querySelector(".notification").setAttribute("user-id", 1);
     console.log(
-        document
-            .querySelector(".notification")
-            .classList.remove("notification_hidden")
+        Number(document.querySelector(".notification").getAttribute("user-id"))
     );
 }
 
