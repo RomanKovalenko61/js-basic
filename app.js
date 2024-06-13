186ef605e3f6ca3f7c59b0fadb8cbdf31714e9d5
Roman <astek14@mail.ru>
2024-06-13 20:44:49 +0300

    17.11. Упражнение - сохранение JSON

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 "use strict";
 
-const obj = JSON.parse('{ "a" : 1 }');
-console.log(obj);
-const str = JSON.stringify(obj);
-console.log(str);
+/* key: 'text', value: { text : 'sdfsdf' } */
+
+function submitForm() {
+    const input = document.querySelector(".input").value;
+    if (!input) {
+        return;
+    }
+    document.querySelector(".panel").innerText = input;
+    document.querySelector(".input").value = "";
+    document
+        .querySelector(".notification")
+        .classList.remove("notification_hidden");
+    const textString = JSON.stringify({
+        text: input,
+    });
+    localStorage.setItem("text", textString);
+}
+
+function inputChanged(e) {
+    if (e.code == "Enter") {
+        submitForm();
+    }
+}
