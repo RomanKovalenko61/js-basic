1b4645d2017b9f778ee43a37d180acbb64dfe5b5
Roman <astek14@mail.ru>
2024-06-23 15:11:06 +0300

    18.16. id привычки в url

--- scripts/app.js
+++ scripts/app.js
@@ -135,6 +135,9 @@ function rerender(activeHabbitId) {
     if (!activeHabbit) {
         return;
     }
+    document.location.replace(
+        document.location.pathname + "#" + activeHabbitId
+    );
     rerenderMenu(activeHabbit);
     rerenderHead(activeHabbit);
     rerenderContent(activeHabbit);
@@ -215,5 +218,11 @@ function addHabbit(event) {
 /* init */
 (() => {
     loadData();
-    rerender(habbits[0].id);
+    const hashId = Number(document.location.hash.replace("#", ""));
+    const urlHabbit = habbits.find((habbit) => habbit.id === hashId);
+    if (urlHabbit) {
+        rerender(urlHabbit.id);
+    } else {
+        rerender(habbits[0].id);
+    }
 })();
